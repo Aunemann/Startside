4637de052a9ffefadca98fbcabe0c3f75455e257
Aunemann <152209611+Aunemann@users.noreply.github.com>
2024-03-07 14:45:01 +0100

    Add files via upload

--- bakgrunn.js
+++ bakgrunn.js
@@ -7,7 +7,7 @@
     "https://i.imgur.com/iWQ6YKo.png",
     "https://i.imgur.com/gnqoRMg.png",
     "https://i.imgur.com/2Tue34r.png",
-    "https://i.imgur.com/F7aMj0m.png"
+    "https://i.imgur.com/F7aMj0m.png",
   ];
 
   var imgCount = images.length;
@@ -18,6 +18,6 @@
 
   var body = document.getElementById('collection-542ba4a1e4b032a0dde82b31');
   body.style.backgroundImage = imgURL;
-  body.style.backgroundSize = "cover";
-  body.style.backgroundRepeat = "no-repeat center center fixed";
+  body.style.backgroundSize = "100% auto";
+  body.style.backgroundRepeat = "no-repeat";
 }());
