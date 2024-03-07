c5a1f8395709783f1c92ee6e69bc523d6dc52667
Aunemann <152209611+Aunemann@users.noreply.github.com>
2024-03-07 14:20:05 +0100

    Update bakgrunn.js

--- bakgrunn.js
+++ bakgrunn.js
@@ -1,18 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var images = [
-      "https://i.imgur.com/fgSn8vr.png",
-      "https://i.imgur.com/GH8pCEG.png",
-      "https://i.imgur.com/kdVXbpw.png"
-    ];
-  
-    var imgCount = images.length;
-  
-    var randNumber = Math.floor(Math.random() * imgCount);
-  
-    var imgURL = "url('" + images[randNumber] + "')";
-  
-    var body = document.getElementById('collection-542ba4a1e4b032a0dde82b31');
-    body.style.backgroundImage = imgURL;
-    body.style.backgroundSize = "cover";
-    body.style.backgroundRepeat = "no-repeat";
-  });
+(function() {
+
+  var images = [
+    "https://i.imgur.com/fgSn8vr.png",
+    "https://i.imgur.com/GH8pCEG.png",
+    "https://i.imgur.com/kdVXbpw.png"
+  ];
+
+  var imgCount = images.length;
+
+  var randNumber = Math.floor((Math.random() * imgCount) + 1);
+
+  imgURL = "url('" + images[randNumber - 1] + "')";
+
+  var body = document.getElementById('collection-542ba4a1e4b032a0dde82b31');
+  body.style.backgroundImage = imgURL;
+  body.style.backgroundSize = "100% auto";
+  body.style.backgroundRepeat = "no-repeat";
+}());
